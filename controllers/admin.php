847ae07e14a16f47378742eeec070a6518f17d24
Ivo Amaral <joseivoamaral@gmail.com>
2012-04-19 23:16:49 -0300

    admin controller

--- controllers/admin.php
+++ controllers/admin.php
@@ -15,25 +15,16 @@ class Admin extends Admin_Controller {
 	 * @return void
 	 */
 	function __construct()
-	{
-	
+	{	
 		// Call the parent's constructor method
 		parent::__construct();		
 		
 		// load Language file
-		$this->lang->load('floorplans');
-		
-		// Get the user ID, if it exists
-		$user = $this->ion_auth->get_user();
-		
-		if(!empty($user))
-		{
-			$this->user_id = $user->id;	
-			$this->config->set_item('user_id', $this->user_id );
-		}else{
-			$this->template->build('admin/access_failed');
-		}
-		
+		$this->lang->load('floorplans');						
 	}
         
+        function index()
+        {
+            //exit('floorplans');
+        }
 }
